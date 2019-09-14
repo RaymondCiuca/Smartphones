dc06227db59bd4a6216333ba7f909c40aa53b506
Mirela Jitianu <mirela.jitianu70@gmail.com>
2019-09-14 10:06:50 +0300

    add new phone

---
+++ src/Iphone11.java
@@ -0,0 +1,16 @@
+public class Iphone11 extends IosSystem {
+    @Override
+    public String getName() {
+        return "Iphone11";
+    }
+
+    @Override
+    public float getPrice() {
+        return 1500f;
+    }
+
+    @Override
+    public String getMemory(){
+        return "128Gb";
+    }
+}

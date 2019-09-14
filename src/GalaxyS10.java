59b98dc97cd83cdcecf4396df963a9195d5d0eb4
Mirela Jitianu <mirela.jitianu70@gmail.com>
2019-09-14 10:06:38 +0300

    add new phone

---
+++ src/GalaxyS10.java
@@ -0,0 +1,16 @@
+public class GalaxyS10 extends AndroidSystem {
+    @Override
+    public String getName() {
+        return "GalaxyS10";
+    }
+
+    @Override
+    public float getPrice() {
+        return 1000f;
+    }
+
+    @Override
+    public String getMemory(){
+        return "512Gb";
+    }
+}
